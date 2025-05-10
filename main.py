6a7857e5941dd6282c6de20268aef05a0e84fcb2
Dexpth_q <65256466+dexpth@users.noreply.github.com>
2025-05-10 21:26:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import telebot
 
-bot = telebot.TeleBot("7961039857:AAEXtgIVlj53ymfZez6RQ3hquRfaRsqE9M4") # You can set parse_mode by default. HTML or MARKDOWN
+bot = telebot.TeleBot("7961039857:AAEhO7O-xUQz8kucrl9b5kgRZjPq475BsPI") # You can set parse_mode by default. HTML or MARKDOWN
 
 @bot.message_handler(commands=['start', 'help'])
 def main(message):
